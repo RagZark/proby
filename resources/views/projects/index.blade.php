648b7122f17c0517b36f3fbb36adeff25102f996
Danver <danver.ver@hotmail.com>
2025-01-30 11:23:42 -0300

    Atualização do template base e estilização do login

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,11 +2,40 @@
 
 @section('title', 'Listagem dos Projetos')
 
+@section('headerContent')
+<nav class="navbar-end">
+    <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link is-link">
+            {{ Auth::user()->name }}
+        </a>
+
+        <div class="navbar-dropdown">
+            <a class="navbar-item">{{ Auth::user()->email }}</a>
+            <a class="navbar-item is-link" href="{{ route('profile.edit') }}">
+                Editar {{ __('Profile') }}
+            </a>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <x-dropdown-link :href="route('logout')" class="is-link"
+                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-dropdown-link>
+            </form>
+        </div>
+    </div>
+    </div>
+</nav>
+@endsection
+
 @section('content')
 
-<h1>Projetos</h1>
+<h1 class="is-size-1 ">Projetos</h1>
 
-<a href="{{ route('projects.create') }}">Novo Projeto</a>
+</br>
+<a href="{{ route('projects.create') }}" class="button is-medium">Novo Projeto + </a>
+</br>
 
 <table class="table">
     <thead>
@@ -14,8 +43,7 @@
             <th>Nome</th>
         </tr>
     <tbody>
-        <tr>
-            @forelse ($projects as $project)
+        @forelse ($projects as $project)
         <tr>
             <td>{{$project->name}}</td>
             <td><a href="{{ route('projects.show', $project->id )}}" class="button is-info">Vizualizar</a></td>
@@ -26,7 +54,6 @@
             <td colspan="100">Nenhum projeto no banco.</td>
         </tr>
         @endforelse
-        </tr>
     </tbody>
     </thead>
 </table>
